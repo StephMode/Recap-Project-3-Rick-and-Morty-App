96e2e710c92f4d93d411cf2d071dbdd7685820cd
Stephan Model <stephan.model@outlook.de>
2024-09-25 11:08:50 +0200

    Add function for creating card element to CharacterCard component

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+// geschweifte Klammern, um den epxort von mehreren Komponenten zu notieren
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -7,6 +10,8 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+
+CharacterCard();
 
 // States
 const maxPage = 1;
