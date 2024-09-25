39f12cfb08b8f378a8d684d9f168baefa8157765
Stephan Model <stephan.model@outlook.de>
2024-09-25 16:16:03 +0200

    Add 1 to improve app behaviour upon search query

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import CharacterCard from "./components/CharacterCard/CharacterCard.js";
 import NavButton from "./components/NavButton/NavButton.js";
 import NavPagination from "./components/NavPagination/NavPagination.js";
 
-
 // geschweifte Klammern, um den epxort von mehreren Komponenten zu notieren
 // ohne geschweifte Klammern, wenn expo default
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -21,15 +22,14 @@ let searchQuery = "";
 const pagination = NavPagination(page, maxPage);
 
 function renderAndAppendCharacterCard() {
-	// const card = CharacterCard(); // entweder const mit value fn call und dann append() const aufnehmen
-	cardContainer.append(CharacterCard()); // oder einfahc die fn selbst als param in den method
-	// return; // brauchen wir nicht, weil
-	// a) keine Daten aus dieser fn an eine andere weitergeben wollen, wir wollen einfahc nur was appenden
-	// b) in append() schon das return implizit ist
+  // const card = CharacterCard(); // entweder const mit value fn call und dann append() const aufnehmen
+  cardContainer.append(CharacterCard()); // oder einfahc die fn selbst als param in den method
+  // return; // brauchen wir nicht, weil
+  // a) keine Daten aus dieser fn an eine andere weitergeben wollen, wir wollen einfahc nur was appenden
+  // b) in append() schon das return implizit ist
 }
 
 async function fetchCharacters() {
-
   const url = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`; // die template literals fügen die let page und searchQuery in die URL für den fetch ein
   const response = await fetch(url);
   const data = await response.json();
@@ -47,19 +47,17 @@ async function fetchCharacters() {
     )
   );
   pagination.textContent = `${page} / ${maxPage}`;
-
 }
 
 fetchCharacters();
 
-
-
 searchBarContainer.append(SearchBar(handleSearch)); // append Search Bar Component
 navigation.append(pagination);
 function handleSearch(query) {
-	searchQuery = query;
-	console.log(searchQuery);
-	fetchCharacters(); //die fn wird hier einfach nur gecalled, weil die URL in fetch characters sich dynamisch durch die temp literals anpasst und im eventLst die let searchQuery
+  searchQuery = query;
+  console.log(searchQuery);
+  page = 1;
+  fetchCharacters(); //die fn wird hier einfach nur gecalled, weil die URL in fetch characters sich dynamisch durch die temp literals anpasst und im eventLst die let searchQuery
 }
 
 // searchBar.addEventListener("submit", (event) => {
@@ -68,7 +66,6 @@ function handleSearch(query) {
 // 	searchQuery = searchResult;
 // 	console.log(searchQuery);
 // 	fetchCharacters(); //die fn wird hier einfach nur gecalled, weil die URL in fetch characters sich dynamisch durch die temp literals anpasst und im eventLst die let searchQuery
-
 
 // nicht vergessen: wenn ich auf das event eines evList zurückgreifen möchte, so muss ich das event als param für die callbakc fn deifnierten
 
